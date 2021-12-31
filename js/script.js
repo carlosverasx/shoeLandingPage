bcf464e2ab9084e70ecc8adfb75563e6b9f157a3
Carlos Augusto Veras <augustov490@gmail.com>
2021-12-31 14:39:19 -0300

    main

--- js/script.js
+++ js/script.js
@@ -7,13 +7,13 @@ menu.onclick = () =>{
     document.body.classList.toggle('active');
 };
 
-window.onscroll = () =>{
-    if (window.innerWidth < 1200){
-        menu.classList.remove('fa-times');
-        header.classList.remove('active');
-        document.body.classList.remove('active');
-    };
-};  
+window.onscroll = () => {
+  if(window.innerWidth < 1200){
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+    document.body.classList.remove('active');
+  };
+};
 
 let productPreviewContainer = document.querySelector('.products-preview-container');
 let productPreview = productPreviewContainer.querySelectorAll('.product-preview');
@@ -40,24 +40,24 @@ document.querySelectorAll('.products-preview-container .product-preview .fa-time
     };
   });
 
-var swiper = new Swiper(".products-slider", {
-    loop: true,
+  var swiper = new Swiper(".products-slider", {
+    loop:true,
     spaceBetween: 20,
-    grabCursor: true,
+    grabCursor:true,
     centeredSlides: true,
     breakpoints: {
       0: {
-        slidesPerView: 1
+        slidesPerView: 1,
       },
       768: {
-        slidesPerView: 2
+        slidesPerView: 2,
       },
       991: {
-        slidesPerView: 3
+        slidesPerView: 3,
       },
     },
   });
-
+  
   var swiper = new Swiper(".reviews-slider", {
     loop:true,
     spaceBetween: 20,
